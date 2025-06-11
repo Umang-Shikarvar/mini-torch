e329d84775aa9bbc527542260b47e527dae9b586
Umang-Shikarvar <143333868+Umang-Shikarvar@users.noreply.github.com>
2025-06-11 17:44:21 +0530

    backpropagation engine

--- engine.py
+++ engine.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 class Tensor:
-    def __init__(self, data, children=(), _op=''):
+    def __init__(self, data, children=(), _op='', requires_grad=True):
         self.data = np.array(data)
         self.grad = np.zeros_like(self.data)
         self._backward = lambda: None
         self.children = set(children)
-        self._op = _op
+        self.op = _op
+        self.requires_grad = requires_grad
 
     def __repr__(self):
         return f"Tensor(data={self.data}, grad={self.grad})"
@@ -14,70 +15,119 @@ class Tensor:
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
 
+        requires_grad = self.requires_grad or other.requires_grad
         try:
-            out = Tensor(self.data + other.data, children=(self, other), _op='+')
+            out = Tensor(self.data + other.data, children=(self, other), _op='+',requires_grad=requires_grad)
         except ValueError as e:
             raise ValueError(f"Tensor shapes {self.data.shape} and {other.data.shape} are not compatible for addition.") from e
 
         def _backward():
-            self_grad = out.grad
-            other_grad = out.grad
-            while self_grad.ndim > self.data.ndim:
-                self_grad = self_grad.sum(axis=0)
-            for  axis,size in enumerate(self.data.shape):
-                if size == 1:
-                    self_grad = self_grad.sum(axis=axis, keepdims=True)
-            while other_grad.ndim > other.data.ndim:
-                other_grad = other_grad.sum(axis=0)
-            for  axis,size in enumerate(other.data.shape):
-                if size == 1:
-                    other_grad = other_grad.sum(axis=axis, keepdims=True)
-            self.grad += self_grad
-            other.grad += other_grad
+            if self.requires_grad:
+                grad_self = out.grad
+                while grad_self.ndim > self.data.ndim:
+                    grad_self = grad_self.sum(axis=0)
+                for axis, size in enumerate(self.data.shape):
+                    if size == 1:
+                        grad_self = grad_self.sum(axis=axis, keepdims=True)
+                self.grad += grad_self
+            if other.requires_grad:
+                grad_other = out.grad
+                while grad_other.ndim > other.data.ndim:
+                    grad_other = grad_other.sum(axis=0)
+                for axis, size in enumerate(other.data.shape):
+                    if size == 1:
+                        grad_other = grad_other.sum(axis=axis, keepdims=True)
+                other.grad += grad_other
         out._backward = _backward
 
         return out
-    
-    def __radd__(self, other):
-        return self+other
     
     def __mul__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
 
+        requires_grad = self.requires_grad or other.requires_grad
         try:
-            out = Tensor(self.data * other.data, children=(self, other), _op='*')
+            out = Tensor(self.data * other.data, children=(self, other), _op='*',requires_grad=requires_grad)
         except ValueError as e:
-            raise ValueError(f"Tensor shapes {self.data.shape} and {other.data.shape} are not compatible for addition.") from e
+            raise ValueError(f"Tensor shapes {self.data.shape} and {other.data.shape} are not compatible for multiplication.") from e
 
         def _backward():
-            self_grad = out.grad * other.data
-            other_grad = out.grad * self.data
-            while self_grad.ndim > self.data.ndim:
-                self_grad = self_grad.sum(axis=0)
-            for  axis,size in enumerate(self.data.shape):
-                if size == 1:
-                    self_grad = self_grad.sum(axis=axis, keepdims=True)
-            while other_grad.ndim > other.data.ndim:
-                other_grad = other_grad.sum(axis=0)
-            for  axis,size in enumerate(other.data.shape):
-                if size == 1:
-                    other_grad = other_grad.sum(axis=axis, keepdims=True)
-            self.grad += self_grad
-            other.grad += other_grad
+            if self.requires_grad:
+                grad_self = out.grad * other.data
+                while grad_self.ndim > self.data.ndim:
+                    grad_self = grad_self.sum(axis=0)
+                for axis, size in enumerate(self.data.shape):
+                    if size == 1:
+                        grad_self = grad_self.sum(axis=axis, keepdims=True)
+                self.grad += grad_self
+            if other.requires_grad:
+                grad_other = out.grad * self.data
+                while grad_other.ndim > other.data.ndim:
+                    grad_other = grad_other.sum(axis=0)
+                for axis, size in enumerate(other.data.shape):
+                    if size == 1:
+                        grad_other = grad_other.sum(axis=axis, keepdims=True)
+                other.grad += grad_other
         out._backward = _backward
 
         return out
+
+    def __pow__(self, power):
+        if not isinstance(power, (int, float)):
+            raise TypeError("Power must be an integer or float.")
+
+        out = Tensor(self.data ** power, children=(self,), _op='**',requires_grad=self.requires_grad)
+
+        def _backward():
+            if self.requires_grad:
+                grad_self = out.grad * power * (self.data ** (power - 1))
+                while grad_self.ndim > self.data.ndim:
+                    grad_self = grad_self.sum(axis=0)
+                for axis, size in enumerate(self.data.shape):
+                    if size == 1:
+                        grad_self = grad_self.sum(axis=axis, keepdims=True)
+                self.grad += grad_self
+        out._backward = _backward
+
+        return out
+
+    def __neg__(self):      # -self
+        return self * -1
     
-    def __rmul__(self, other):
+    def __sub__(self, other):       # self - other
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return self + (-other)
+    
+    def __radd__(self, other):      # other + self
+        return self+other
+    
+    def __rsub__(self, other):      # other - self
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return other + (-self)
+    
+    def __rmul__(self, other):      # other * self
         return self * other
+
+    def __truediv__(self, other):       # self / other
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return self * (other ** -1)
+
+    def __rtruediv__(self, other):      # other / self
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        return other * (self ** -1)
+    
+
+
     
     def backward(self):
-        self.grad= np.ones_like(self.data)
+        if not self.requires_grad:
+            raise RuntimeError("Cannot call backward on a tensor that does not require gradients.")
+        self.grad = np.ones_like(self.data)
 
-        topo= []
+        topo = []
         visited = set()
         def topo_sort(tensor):
-            if(tensor not in visited):
+            if tensor not in visited:
                 visited.add(tensor)
                 for child in tensor.children:
                     topo_sort(child)
