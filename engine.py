798e10579657c673ae4e3b271f044a16d8316e64
Shardul Junagade <23110297@iitgn.ac.in>
2025-06-11 23:38:55 +0530

    add matmul

--- engine.py
+++ engine.py
@@ -2,8 +2,8 @@ import numpy as np
 
 class Tensor:
     def __init__(self, data, children=(), _op='', requires_grad=True):
-        self.data = np.array(data)
-        self.grad = np.zeros_like(self.data)
+        self.data = np.array(data, dtype=float)
+        self.grad = np.zeros_like(self.data, dtype=float)
         self._backward = lambda: None
         self.children = set(children)
         self._op = _op
@@ -163,13 +163,45 @@ class Tensor:
         return other * (self ** -1)
     
 
+    def __matmul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        
+        requires_grad = self.requires_grad or other.requires_grad
+        out_data = np.matmul(self.data, other.data)
+        out = Tensor(out_data, children=(self, other), requires_grad=requires_grad)
 
+        def _backward():
+            if self.requires_grad:
+                grad_self = np.matmul(out.grad, np.swapaxes(other.data, -1, -2))
+                # Handle broadcasting during backprop
+                while grad_self.ndim > self.data.ndim:
+                    grad_self = grad_self.sum(axis=0)
+                for axis, size in enumerate(self.data.shape):
+                    if size == 1:
+                        grad_self = grad_self.sum(axis=axis, keepdims=True)
+                self.grad += grad_self
+
+            if other.requires_grad:
+                grad_other = np.matmul(np.swapaxes(self.data, -1, -2), out.grad)
+                while grad_other.ndim > other.data.ndim:
+                    grad_other = grad_other.sum(axis=0)
+                for axis, size in enumerate(other.data.shape):
+                    if size == 1:
+                        grad_other = grad_other.sum(axis=axis, keepdims=True)
+                other.grad += grad_other
+
+        out._backward = _backward
+        return out
 
     
-    def backward(self):
+    def backward(self, grad=None):
         if not self.requires_grad:
             raise RuntimeError("Cannot call backward on a tensor that does not require gradients.")
-        self.grad = np.ones_like(self.data)
+        if grad is None:
+            grad = np.ones_like(self.data, dtype=float)
+        else:
+            grad = np.array(grad, dtype=float)
+        self.grad = grad
 
         topo = []
         visited = set()
