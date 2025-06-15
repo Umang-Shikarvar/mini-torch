932f77087f63aa5bdb4c0164bb52b57e76a3f2dc
Shardul Junagade <23110297@iitgn.ac.in>
2025-06-15 15:48:29 +0530

    fix shape of weights in Linear layer as per official torch

--- minitorch/nn/modules/linear.py
+++ minitorch/nn/modules/linear.py
@@ -5,14 +5,13 @@ from minitorch.nn.parameter import Parameter
 from .module import Module
 
 class Linear(Module):
-    def __init__(self,in_features, out_features, bias=True) -> None:
+    def __init__(self, in_features, out_features, bias=True) -> None:
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        k= 1 / (in_features ** 0.5)
-        
-        weight_data = [[random.uniform(-k, k) for _ in range(out_features)] for _ in range(in_features)]
-        self.weight = Parameter(minitorch.Tensor(weight_data)) 
+        k = 1 / (in_features ** 0.5)
+        weight_data = [[random.uniform(-k, k) for _ in range(in_features)] for _ in range(out_features)]
+        self.weight = Parameter(minitorch.Tensor(weight_data))
 
         if bias:
             bias_data = [random.uniform(-k, k) for _ in range(out_features)]
@@ -20,17 +19,16 @@ class Linear(Module):
         else:
             self.bias = None
 
-    
     def forward(self, x):
         if not isinstance(x, minitorch.Tensor):
             raise TypeError("Input must be a Tensor.")
         if x.data.shape[1] != self.in_features:
             raise ValueError(f"Input shape {x.data.shape} does not match expected shape ({x.data.shape[0]}, {self.in_features}).")
-        
-        out = x @ self.weight
+        out = x @ self.weight.T
         if self.bias is not None:
-            out += self.bias
+            out = out + self.bias
         return out
-    
+
     def __repr__(self):
         return f"Linear(in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None})"
+
