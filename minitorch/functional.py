d15744e91996a035db571be6d149bd755f4b9026
Shardul Junagade <23110297@iitgn.ac.in>
2025-06-21 18:40:34 +0530

    update activation funcs

--- minitorch/functional.py
+++ minitorch/functional.py
@@ -53,11 +53,13 @@ def transpose(tensor, dim0, dim1):
 
 def relu(tensor):
     tensor = tensor if isinstance(tensor, Tensor) else Tensor(tensor)
-    out = Tensor(np.maximum(0, tensor.data), children=(tensor,), _op='relu', requires_grad=tensor.requires_grad)
+    relu_val = np.maximum(0, tensor.data)
+    out = Tensor(relu_val, children=(tensor,), _op='relu', requires_grad=tensor.requires_grad)
 
     def _backward():
         if tensor.requires_grad:
-            grad_self = out.grad * (tensor.data > 0)
+            mask = (tensor.data > 0).astype(tensor.data.dtype)
+            grad_self = out.grad * mask
             grad_self = _handle_broadcasting(grad_self, tensor.data.shape)
             tensor.grad += grad_self
     out._backward = _backward
@@ -66,12 +68,12 @@ def relu(tensor):
 
 def sigmoid(tensor):
     tensor = tensor if isinstance(tensor, Tensor) else Tensor(tensor)
-    sig = 1 / (1 + np.exp(-tensor.data))
-    out = Tensor(sig, children=(tensor,), _op='sigmoid', requires_grad=tensor.requires_grad)
+    sig_val = 1 / (1 + np.exp(-tensor.data))
+    out = Tensor(sig_val, children=(tensor,), _op='sigmoid', requires_grad=tensor.requires_grad)
 
     def _backward():
         if tensor.requires_grad:
-            grad_self = out.grad * sig * (1 - sig)
+            grad_self = out.grad * out.data * (1 - out.data)
             grad_self = _handle_broadcasting(grad_self, tensor.data.shape)
             tensor.grad += grad_self
     out._backward = _backward
@@ -85,7 +87,7 @@ def tanh(tensor):
 
     def _backward():
         if tensor.requires_grad:
-            grad_self = out.grad * (1 - tanh_val ** 2)
+            grad_self = out.grad * (1 - out.data ** 2)
             grad_self = _handle_broadcasting(grad_self, tensor.data.shape)
             tensor.grad += grad_self
     out._backward = _backward
