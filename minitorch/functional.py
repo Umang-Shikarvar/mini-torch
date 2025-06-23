fix softmax function

minitorch/functional.py
@@ -140,13 +140,24 @@ def softmax(tensor, dim=-1):
 
     def _backward():
         if tensor.requires_grad:
-            grad_self = np.empty_like(out.data)
-            for i in range(out.data.shape[0]):
-                s = out.data[i].reshape(-1, 1)
-                jacobian = np.diagflat(s) - np.dot(s, s.T)
-                grad_self[i] = np.dot(jacobian, out.grad[i])
+
+            ## previous code using jacobian
+            # grad_self = np.empty_like(out.data)
+            # for i in range(out.data.shape[0]):
+            #     s = out.data[i].reshape(-1, 1)
+            #     jacobian = np.diagflat(s) - np.dot(s, s.T)
+            #     grad_self[i] = np.dot(jacobian, out.grad[i])
+
+
+            # grad shape: same as out.data
+            grad = out.grad
+            s = out.data
+            # sum over the softmax axis
+            dot = np.sum(grad * s, axis=dim, keepdims=True)
+            grad_self = s * (grad - dot)
             grad_self = _handle_broadcasting(grad_self, tensor.data.shape)
             tensor.grad += grad_self
+            
     out._backward = _backward
 
     return out